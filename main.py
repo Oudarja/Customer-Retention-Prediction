bbba01d91a1237a206f004621c091f1eac20077e
oudarja <oudarja360@gmail.com>
2025-10-16 17:35:18 +0600

    training and evaluation finally done

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.data_preprocessing import  encode_features, scale_features_stdscale, scale_features_minmax
 from src.feature_engineering import add_features
 import pandas as pd
+from src.model_evaluation import evaluate_model, evaluate_keras_model
 
-from utils.utils import load_data, save_model_or_data, load_model, timer
+from utils.utils import load_data, save_model_or_data,load_any_model
 
 df = load_data('data/raw_data/dataset.csv')
 
@@ -12,5 +13,25 @@ save_model_or_data(df, 'data/processed_data/processed_data.csv')
 
 df = add_features(df)
 
-save_model_or_data(df, 'data/processed_data/processed_data_with_engineered_features1.csv')
+# model loading 
+
+rf = load_any_model('models/randomforest_best.pkl')
+xgb = load_any_model('models/xgboost_best.pkl')
+lgbm = load_any_model('models/lgbm_best.pkl')
+ann = load_any_model('models/ann_best.h5')
+
+test_x_rf= load_data('data/processed_data/X_test_final_rf.csv')
+test_x_xgb= load_data('data/processed_data/X_test_final_xgb.csv')   
+test_x_lgbm= load_data('data/processed_data/X_test_final_lgbm.csv')
+test_x_ann= load_data('data/processed_data/X_test_final_ann.csv')
+test_y= load_data('data/processed_data/y_test.csv')
+
+evaluate_model(rf, test_x_rf, test_y, "Random Forest")
+
+evaluate_model(xgb, test_x_xgb, test_y, "XGBoost")
+
+evaluate_model(lgbm, test_x_lgbm, test_y, "LightGBM")
+
+evaluate_keras_model(ann, test_x_ann, test_y, "ANN")
+
 
--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,9 +1,47 @@
 from sklearn.metrics import classification_report, roc_auc_score, confusion_matrix, RocCurveDisplay
-
+import os
+import matplotlib.pyplot as plt
 def evaluate_model(model, X_test, y_test, model_name):
     y_pred = model.predict(X_test)
     print(f"=== {model_name} ===")
     print(classification_report(y_test, y_pred))
+    print('------------------------------------')
     print("ROC-AUC:", roc_auc_score(y_test, y_pred))
+    print('------------------------------------')
+    print("Confusion Matrix:")
     cm = confusion_matrix(y_test, y_pred)
-    return cm
+    print(cm)
+    print('------------------------------------')
+    # --- Plot and save ROC curve ---
+    plt.figure()
+    RocCurveDisplay.from_predictions(y_test, y_pred)
+    plt.title(f"ROC Curve - {model_name}")
+    
+    save_path = f"fig/{model_name}_roc_curve.png"
+    plt.savefig(save_path, bbox_inches="tight")
+    plt.close()
+    
+    print(f"ROC curve saved to: {save_path}")
+
+def evaluate_keras_model(model, X_test, y_test, model_name):
+    y_pred_prob = model.predict(X_test)
+    y_pred = (y_pred_prob >= 0.5).astype("int32")
+    print(f"=== {model_name} ===")
+    print(classification_report(y_test, y_pred))
+    print('------------------------------------')
+    print("ROC-AUC:", roc_auc_score(y_test, y_pred))
+    print('------------------------------------')
+    print("Confusion Matrix:")
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    print('------------------------------------')
+    # --- Plot and save ROC curve ---
+    plt.figure()
+    RocCurveDisplay.from_predictions(y_test, y_pred)
+    plt.title(f"ROC Curve - {model_name}")
+    
+    save_path = f"fig/{model_name}_roc_curve.png"
+    plt.savefig(save_path, bbox_inches="tight")
+    plt.close()
+    
+    print(f"ROC curve saved to: {save_path}")
